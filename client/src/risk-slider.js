f3120be3831fa25dd0f3daa244e57fc21592277d
ngittlen <nggittle@med.umich.edu>
2021-03-22 14:13:47 -0400

    Front end now communicates with an activator to get the ko resource containing the js code that has the calculations

--- client/src/risk-slider.js
+++ client/src/risk-slider.js
@@ -9,6 +9,8 @@ let patient = {
 }
 let score = 0;
 
+// Frontend data transfer code
+
 document.getElementById("strokerisk").addEventListener("change",
     () => {
         let risk = getStrokeRiskFunction()(document.getElementById("strokerisk").value, score);
@@ -34,54 +36,4 @@ function generatePatientProfile() {
     score = generateScore(patient)
     document.getElementById("score").innerText = "Your risk score is " + score;
 
-}
-
-// Ko below vvv
-
-function generateScore(patient) {
-    let score = 0;
-    if(patient.age > 65 && patient.age < 75){
-        score += 1;
-    } else if (patient.age >= 75) {
-        score += 2;
-    }
-    if (patient.female) {
-        score += 1;
-    }
-    if (patient.chf) {
-        score += 1;
-    }
-    if (patient.diabetes) {
-        score += 1;
-    }
-    if (patient.hypertension) {
-        score += 1;
-    }
-    if (patient.stroke) {
-        score += 2;
-    }
-    if (patient.vasc) {
-        score += 1;
-    }
-    return score;
-}
-
-function getRiskCoefficient(score) {
-    if (score < 3) {
-        return 1;
-    } else {
-        return 2;
-    }
-}
-
-function getRiskConstant(score) {
-    if (score < 5) {
-        return 0;
-    } else {
-        return 50;
-    }
-}
-
-function getStrokeRiskFunction() {
-    return (preference, score) => preference * getRiskCoefficient(score) + parseInt(getRiskConstant(score), 10);
 }
